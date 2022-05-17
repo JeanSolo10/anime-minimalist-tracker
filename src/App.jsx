09dc9846dc9324476b03ec041b5f18bf51e8b5bf
JeanSolo <jeanpksolo@gmail.com>
2022-05-17 10:47:42 -0400

    feat: add routes app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import { AuthContextProvider } from "./context/AuthContext";
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
+import ForgotPassword from "./components/ForgotPassword";
+import Dashboard from "./components/Dashboard";
+import axios from "axios";
 
 function App() {
   const [herokuTest, setHerokuTest] = useState("");
@@ -17,9 +23,14 @@ function App() {
 
   return (
     <>
-      <div className="App">
-        <h1>Hello {herokuTest}</h1>
-      </div>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/login" element={<Signin />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/" element={<Dashboard />}></Route>
+          <Route path="/forgot-password" element={<ForgotPassword />}></Route>
+        </Routes>
+      </AuthContextProvider>
     </>
   );
 }
