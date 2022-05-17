e23c42aa8e0c195870bb30c56bfaea8bf969970c
JeanSolo <jeanpksolo@gmail.com>
2022-05-17 10:36:17 -0400

    feat: add AuthContext for firebase

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,54 @@
+import { auth } from "../firebase";
+import { useState, useEffect, createContext, useContext } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+} from "firebase/auth";
+
+const userContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState({});
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logout = () => {
+    return signOut(auth);
+  };
+
+  const resetPassword = (email) => {
+    return sendPasswordResetEmail(auth, email);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const values = {
+    createUser,
+    logout,
+    signIn,
+    resetPassword,
+    user,
+  };
+
+  return <userContext.Provider value={values}>{children}</userContext.Provider>;
+};
+
+export const UserAuth = () => {
+  return useContext(userContext);
+};
