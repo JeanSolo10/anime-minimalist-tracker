0a7f2c026746fe89bc1ecbb05df5495ca408ea8b
JeanSolo <jeanpksolo@gmail.com>
2022-05-18 14:50:44 -0400

    feat: add initial migration for heroku

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const app = require("./app");
+const db = require("./knex");
 
 const PORT = process.env.PORT || 5000;
 
 (async () => {
   try {
+    await db.migrate.latest();
     app.listen(PORT, () => {
       console.log(`App listening on port ${PORT}!`);
     });
