86df844e7cdbb1977605b3f13154f9dd9c325045
JeanSolo <jeanpksolo@gmail.com>
2022-05-19 07:12:48 -0400

    refactor: update single view

--- src/components/AnimeSingleView.jsx
+++ src/components/AnimeSingleView.jsx
@@ -6,13 +6,16 @@ import {
 import { useSelector, useDispatch } from "react-redux";
 import axios from "axios";
 import "../styles/AnimeSingleView.css";
+import { UserAuth } from "../context/AuthContext";
 
 export default function AnimeSingleView() {
   const [anime, setAnime] = useState([]);
+  const [error, setError] = useState("");
   const placeholderImage = require("../images/anime_placeholder.jpg");
   /* redux */
   const { currentSeasonAnimes, selectedAnimeIndex, animeInUserWatchList } =
     useSelector((state) => state.animes);
+  const { user } = UserAuth();
 
   useEffect(() => {
     setAnime(currentSeasonAnimes[selectedAnimeIndex]);
@@ -21,6 +24,10 @@ export default function AnimeSingleView() {
   const dispatch = useDispatch();
 
   const handleButtonClick = (action, id) => {
+    const authToken = localStorage.getItem("Auth Token");
+    if (!authToken && !user) {
+      return setError("You must be logged in to save anime!");
+    }
     dispatch(updateAnimeInUserWatchList(id));
   };
 
@@ -41,7 +48,7 @@ export default function AnimeSingleView() {
                 />
               </div>
               <div className="anime-info">
-                {/* <div className="anime-score">
+                <div className="anime-score">
                   <p>
                     <span className="anime-info-sub-title">SCORE: </span>
                     {anime.score}
@@ -52,23 +59,24 @@ export default function AnimeSingleView() {
                     <span className="anime-info-sub-title">EPISODES: </span>
                     {anime.episodes}
                   </p>
-                </div> */}
-                {/* <div className="anime-genres">
+                </div>
+                <div className="anime-genres">
                   <span className="anime-info-sub-title">GENRES: </span>
                   {anime &&
-                    anime.genres.map((genre, index) => (
+                    anime.genres &&
+                    anime.genres.genreTypes.map((genre, index) => (
                       <span className="anime-genre" key={index}>
-                        {(index ? "," : "") + " " + genre.name}
+                        {(index ? "," : "") + " " + genre}
                       </span>
                     ))}
-                </div> */}
+                </div>
                 <div className="anime-add-to-list-container">
                   {animeInUserWatchList[anime.id] ? (
                     <button
                       onClick={() => handleButtonClick("remove", anime.id)}
                       className="anime-remove-from-list-btn"
                     >
-                      Remove from watch list
+                      Remove from list
                     </button>
                   ) : (
                     <button
@@ -81,7 +89,7 @@ export default function AnimeSingleView() {
                 </div>
               </div>
             </div>
-
+            {error && <div className="error-message">{error}</div>}
             <div className="anime-synopsis">
               <h3>Sypnosis</h3>
               <p>{anime.synopsis}</p>
