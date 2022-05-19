d4bd79a0e1c2a73e7ab46dc6c1930352dd47b0b6
JeanSolo <jeanpksolo@gmail.com>
2022-05-18 23:37:53 -0400

    refactor: remove testing seeds

--- db/seeds/003-add-anime.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.seed = async function (knex) {
-  // Deletes ALL existing entries
-  await knex("animes").del();
-  await knex("animes").insert([{ id: 1, name: "Anime Test" }]);
-};
