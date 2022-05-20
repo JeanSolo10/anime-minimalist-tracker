aad7f03a06663a6346eff715bc2da4f03086a878
JeanSolo <jeanpksolo@gmail.com>
2022-05-20 08:08:25 -0400

    fix: remove seeding for heroku deployment

--- backend/server/index.js
+++ backend/server/index.js
@@ -7,7 +7,6 @@ const PORT = process.env.PORT || 5000;
 (async () => {
   try {
     await db.migrate.latest();
-    await db.seed.run();
     app.listen(PORT, () => {
       console.log(`App listening on port ${PORT}!`);
     });
