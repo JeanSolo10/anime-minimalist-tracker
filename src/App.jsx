bed6901cb416753b32545d07c6b92d711390b5fa
JeanSolo <jeanpksolo@gmail.com>
2022-05-22 18:46:42 -0400

    feat: add logic to scroll to top of page

--- src/App.jsx
+++ src/App.jsx
@@ -10,22 +10,31 @@ import AnimeSingleView from "./components/AnimeSingleView";
 import MyWatchList from "./components/MyWatchList";
 import Users from "./components/Users";
 import UserAnimeList from "./components/UserAnimeList";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <>
       <AuthContextProvider>
-        <Routes>
-          <Route path="/login" element={<Signin />}></Route>
-          <Route path="/signup" element={<Signup />}></Route>
-          <Route path="/" element={<Dashboard />}></Route>
-          <Route path="/forgot-password" element={<ForgotPassword />}></Route>
-          <Route path="/seasons/:season" element={<AllAnimeBySeason />}></Route>
-          <Route path="/anime/:id/:name" element={<AnimeSingleView />}></Route>
-          <Route path="/animewatchlist" element={<MyWatchList />}></Route>
-          <Route path="/users" element={<Users />}></Route>
-          <Route path="/users/:username" element={<UserAnimeList />}></Route>
-        </Routes>
+        <ScrollToTop>
+          <Routes>
+            <Route path="/login" element={<Signin />}></Route>
+            <Route path="/signup" element={<Signup />}></Route>
+            <Route path="/" element={<Dashboard />}></Route>
+            <Route path="/forgot-password" element={<ForgotPassword />}></Route>
+            <Route
+              path="/seasons/:season"
+              element={<AllAnimeBySeason />}
+            ></Route>
+            <Route
+              path="/anime/:id/:name"
+              element={<AnimeSingleView />}
+            ></Route>
+            <Route path="/animewatchlist" element={<MyWatchList />}></Route>
+            <Route path="/users" element={<Users />}></Route>
+            <Route path="/users/:username" element={<UserAnimeList />}></Route>
+          </Routes>
+        </ScrollToTop>
       </AuthContextProvider>
     </>
   );
